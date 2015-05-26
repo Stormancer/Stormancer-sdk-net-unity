07f56f6da205e1edd11876d2d7f0571a08b5058b
jmderuty <jmderuty@stormancer.com>
2015-05-26 15:24:11 +0200

    Added IEnvironment component

---
+++ src/Server/Stormancer.Server/Components/HostStatus.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stormancer.Server.Components
+{
+    /// <summary>
+    /// Informations about the running application
+    /// </summary>
+    public class ApplicationInfos
+    {
+        /// <summary>
+        /// A boolean value indicating whether the current host is the active deployment.
+        /// </summary>
+        public bool IsActiveDeployment { get; set; }
+
+        /// <summary>
+        /// A string containing the id of the current deployment.
+        /// </summary>
+        public string DeploymentId { get; set; }
+
+        /// <summary>
+        /// A string containing the name of the current application.
+        /// </summary>
+        public string ApplicationName { get; set; }
+
+        /// <summary>
+        /// A string containing the id of the account containing the application.
+        /// </summary>
+        public string AccountId { get; set; }
+
+        /// <summary>
+        /// A string containing the primary access key of the application.
+        /// </summary>
+        public string PrimaryKey { get; set; }
+
+        /// <summary>
+        /// A string containing the secondary access key of the application.
+        /// </summary>
+        public string SecondaryKey { get; set; }
+    }
+}
---
+++ src/Server/Stormancer.Server/Components/IEnvironment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stormancer.Server.Components
+{
+    /// <summary>
+    /// Contains methods to get in
+    /// </summary>
+    public interface IEnvironment
+    {
+        /// <summary>
+        /// Returns informations about the current application.
+        /// </summary>
+        /// <returns>A Task returning an ApplicationInfos instance on completion.</returns>
+        Task<ApplicationInfos> GetApplicationInfos();
+
+        /// <summary>
+        /// Event fired when the active deployment change.
+        /// </summary>
+        event EventHandler<ActiveDeploymentChangedEventArgs> ActiveDeploymentChanged;
+
+    }
+
+    /// <summary>
+    /// Contains informations about the new active deployment when the ActiveDeploymentChanged event is fired.
+    /// </summary>
+    public class ActiveDeploymentChangedEventArgs:EventArgs
+    {
+        /// <summary>
+        /// A boolean value indicating whether the current deployment is now active.
+        /// </summary>
+        public bool IsActive{get;set;}
+
+        /// <summary>
+        /// A string containing the id of the active deployment.
+        /// </summary>
+        public string ActiveDeploymentId{get;set;}
+    }
+}
